18648d7e2220f6294185f77f3e2696aad554d77a
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2022-09-21 16:03:16 -0400

    corrected method of access to ace archive directories

--- ace_nh_viol.py
+++ ace_nh_viol.py
@@ -6,12 +6,32 @@ import sys
 import os
 from datetime import datetime
 
-#
-#--Creates file to store log of steps
-#
-path = "/tmp/mta/ace_nh_alert"#path for testing this on the mta user of desktop machine scrapper. change with more info
 
-trackdir = path + "/tracker"
+#
+#--- reading directory list
+#
+path = '/data/mta4/Space_Weather/house_keeping/dir_list'
+
+with open(path, 'r') as f:
+    data = [line.strip() for line in f.readlines()]
+
+for ent in data:
+    atemp = re.split(':', ent)
+    var  = atemp[1].strip()
+    line = atemp[0].strip()
+    exec( "%s = %s" %(var, line))
+
+#
+#--- append  pathes to private folders to a python directory
+#
+sys.path.append('/data/mta4/Script/Python3.8/MTA/')
+
+#
+#--Create file to store log of steps
+#
+pathTest = "/home/waaron/git/ace_nh_alert" #path for testing this script on desktop machine scrapper. remove psot testring
+#creating a tracking file for the testing progress of this script as it runs on the the desktop machine scrapper.
+trackdir = pathTest + "/tracker"
 if (not os.path.exists(trackdir)):
     os.system(f'mkdir {trackdir}')
 
@@ -26,15 +46,15 @@ log_handle = open(trackdir + "/ace_nh_viol.log","w+")#opens a log file to record
 
 if (len(sys.argv) == 1):#if no arguements passed
     viol_hour = 8#defaults to alert after 8 hours
-    log_handle.write("No Arg Passed: viol_hour=8\n")
+    log_handle.write("No Arg Passed: viol_hour=8\n")#remove post testing
 else:
     viol_hour = int(sys.argv[1]) #if arguements passed
-    log_handle.write(f'Arg Passed: viol_hour={viol_hour}\n')
+    log_handle.write(f'Arg Passed: viol_hour={viol_hour}\n')#remove post testing
     #TODO: error handling of non-int arguement of multiple arguements
 
 
 if (viol_hour >= 13):
-    log_handle.write(f'Error:Violation hour until notification: {viol_hour}h: exceeds 12h.\n')
+    log_handle.write(f'Error:Violation hour until notification: {viol_hour}h: exceeds 12h.\n')#remove post testing
     raise Exception(f'Error: Violation hour until notification: {viol_hour}h: exceeds 12h.')
 
 #
@@ -42,16 +62,16 @@ if (viol_hour >= 13):
 #
 
 
-infile = "/data/mta4/Space_Weather/ACE/Data/ace_12h_archive"#change post testing and with more info
+infile = ace_dir +  "Data/ace_12h_archive"
 archive_length_lim = 12 * viol_hour # 12 five-min segments per hour.
 
 
 
-lockdir = path + "/viol"#change post testing and with more info
+lockdir = pathTest + "/viol"#change post testing and with more info
 
 if (not os.path.exists(lockdir)):
     os.system(f'mkdir {lockdir}')
-    log_handle.write(f'{lockdir} dir does not exist. Creating.\n')
+    log_handle.write(f'{lockdir} dir does not exist. Creating.\n')#remvoe post testing
 
 lockfile = lockdir + "/ace_nh_viol.out"
 
@@ -67,37 +87,37 @@ count = 0 #counter for
 for line in reversed(list(open(infile))):#iterating over the file lines in reverse
     data = line.split()
     
-    log_handle.write(f'Reverse Line iterating: Line: {count}, Time: {data[3]}\n')#lines incoporated such that the time should step in reverse
+    log_handle.write(f'Reverse Line iterating: Line: {count}, Time: {data[3]}\n')#lines incoporated such that the time should step in reverse. remove post testing
 
     if (count >= archive_length_lim):#break for loop after exceeding viol_hour
-        log_handle.write(f'Iterated till viol_hour: Breaking Loop A\n')
+        log_handle.write(f'Iterated till viol_hour: Breaking Loop A\n')#remove post testing
         break
     
     
     #electrons
     if (data[6] != "0"):
         bad_e_data = bad_e_data + 1
-        log_handle.write(f'Data[6]: {data[6]} :Increment bad_e_data\n')
+        log_handle.write(f'Data[6]: {data[6]} :Increment bad_e_data\n')#remove post testing
     #protons
     if (data[9] != "0"):
         bad_p_data = bad_p_data + 1
-        log_handle.write(f'Data[9]: {data[9]} :Increment bad_p_data\n')
+        log_handle.write(f'Data[9]: {data[9]} :Increment bad_p_data\n')#remove post testing
     
     count = count + 1 # final step, to increment counter
 
 
-log_handle.write(f'Total bad data count: E: {bad_e_data}, P: {bad_p_data}\n')
+log_handle.write(f'Total bad data count: E: {bad_e_data}, P: {bad_p_data}\n')#remove post testing
 #
 #--Alert Check: Does not send alerts between midnight and 7 am
 #
 hour_now = int(datetime.now().strftime("%H"))#get current hour
-log_handle.write(f'Current Hour: {hour_now}\n')
+log_handle.write(f'Current Hour: {hour_now}\n')#remove post testing
 if ((hour_now < 24) and (hour_now > 7)):
-    log_handle.write(f'Within Alert Time\n')
+    log_handle.write(f'Within Alert Time\n')#remove post testing
 
     if ((bad_e_data == archive_length_lim) or (bad_p_data == archive_length_lim)):
         #No valid data for viol_hour hours, send out an alert if it has not been sent out yet
-        log_handle.write(f'Enough Bad Data Found: Uh-Oh. \n')
+        log_handle.write(f'Enough Bad Data Found: Uh-Oh. \n')#remove post testing
         #writing the lock file
         if (os.path.exists(lockfile)):
             os.system(f'date >> {lockfile}')#touch lockfile, updating the date
@@ -116,4 +136,4 @@ if ((hour_now < 24) and (hour_now > 7)):
             os.system(f'cp {infile} {lockdir+"/ace_12h_archive_alert"}')
 
             
-log_handle.close(), #closes log operations file
+log_handle.close(), #closes log operations file. remove post testing
